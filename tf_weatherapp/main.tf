45eed4280c40e78b3bd34148c1c36ffc044208e9
ruwendra91 <ruwendra91@gmail.com>
2024-07-01 00:24:24 +0530

    Update main.tf

--- tf_weatherapp/main.tf
+++ tf_weatherapp/main.tf
@@ -69,7 +69,7 @@ resource "aws_lb_listener" "nlb_listener" {
 
 resource "aws_launch_configuration" "weather_lc" {
   name_prefix          = "weather_lc_"
-  image_id             = "ami-0592b275eb216ecfb"
+  image_id             = "ami-00f40b76164a90c56"
   instance_type        = "t3.micro"
   security_groups      = [aws_security_group.weather_instance_sg.id]
   iam_instance_profile = data.aws_iam_instance_profile.existing_profile.arn
